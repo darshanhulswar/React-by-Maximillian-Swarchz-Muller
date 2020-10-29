f4545445a3457ab353f69d8ce36c98a86c29baa6
Darshan <dhulswar591@gmail.com>
2020-10-29 15:39:22 +0530

    Using Media Queries in Radium by using StyleRoot Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Person from "./Person/Person";
-import Radium from 'radium'; // import Radium
+import Radium, { StyleRoot } from 'radium'; // import Radium
 import "./App.css";
 
 class App extends Component {
@@ -93,17 +93,19 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >
-          Toggle Persons
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={style}
+            onClick={this.togglePersonsHandler}
+          >
+            Toggle Persons
         </button>
-        {persons}
-      </div>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }

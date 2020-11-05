db538559133ccdb767606d8972aba55992f344ff
Darshan <dhulswar591@gmail.com>
2020-11-05 22:25:54 +0530

    Persons out-sourced from App.js

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Person from './Person/Person'
 
-function Persons() {
-    return (
-        <div>
-
-        </div>
-    )
-}
+const Persons = (props) => props.persons.map((person, index) => {
+    return <Person
+        click={() => props.clicked(index)}
+        name={person.name}
+        age={person.age}
+        key={person.id}
+        changeName={(event) => props.changed(event, person.id)} />
+})
 
 export default Persons

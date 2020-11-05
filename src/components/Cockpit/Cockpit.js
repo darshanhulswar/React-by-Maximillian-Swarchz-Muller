c73f0b231e928239c2caf97acf33f0a3760ad453
Darshan <dhulswar591@gmail.com>
2020-11-05 23:06:03 +0530

    Cockpit component out-sourced from App.js

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import CockpitClasses from './Cockpit.module.css'
+
+const Cockpit = props => {
+
+    const classes = [];
+    let btnClass = CockpitClasses.btn
+
+    if (props.showPersons) {
+        btnClass = CockpitClasses.btnDanger
+    }
+
+    if (props.personsLength <= 2) {
+        classes.push(CockpitClasses.red);
+    }
+
+    if (props.personsLength <= 1) {
+        classes.push(CockpitClasses.bold);
+    }
+
+    return (
+        <div className={CockpitClasses.Cockpit}>
+            <h1>Hi, I'm a React App</h1>
+            <p className={classes.join(' ')}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.togglePersonsHandler}
+            >
+                Toggle Persons
+        </button>
+        </div>
+    )
+}
+
+export default Cockpit

74886af4686511606c5282ca624841d83c92cab9
Darshan <dhulswar591@gmail.com>
2020-11-06 23:43:42 +0530

    props received from App.js is set out on Cockpit component (App Header)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -20,7 +20,7 @@ const Cockpit = props => {
 
     return (
         <div className={CockpitClasses.Cockpit}>
-            <h1>Hi, I'm a React App</h1>
+            <h1>{props.title}</h1>
             <p className={classes.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}

20d0f06aef9a38dc40ad890e10991a6dee4b49dd
Darshan <dhulswar591@gmail.com>
2020-11-07 23:01:28 +0530

    Component Life Cycle - Persons component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Person from './Person/Person'
 
-const Persons = (props) => props.persons.map((person, index) => {
-    return <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changeName={(event) => props.changed(event, person.id)} />
-})
+const Persons = (props) => {
+    console.log('[Persons.js] rendering...');
+    props.persons.map((person, index) => {
+        return <Person
+            click={() => props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changeName={(event) => props.changed(event, person.id)} />
+    })
+}
 
 export default Persons

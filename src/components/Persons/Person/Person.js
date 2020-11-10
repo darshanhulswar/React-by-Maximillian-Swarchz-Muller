b8c8bc8d081b5c32cdc15e0a801eb4dd228908b6
Darshan <dhulswar591@gmail.com>
2020-11-10 11:04:02 +0530

    Wrapping children inside <Aux> Higher Order Component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-import personStyle from './Person.module.css';
+import Aux from '../../../hoc/Auxiliary';
+// import personStyle from './Person.module.css';
 
 export class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={personStyle.Person}>
+            <Aux>
                 <p onClick={this.props.click}>Hello I'm {this.props.name} and I'm {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changeName} value={this.props.name} />
-            </div>
+            </Aux>
         )
+
+
     }
 }
 

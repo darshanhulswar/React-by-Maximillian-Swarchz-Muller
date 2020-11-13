aab106905ea4e1766604a636096ddc66530c8ceb
Darshan <dhulswar591@gmail.com>
2020-11-13 20:17:04 +0530

    WithClass higher order component addition

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component, Fragment } from 'react'
+// import Aux from '../../../hoc/Auxiliary';
 // import personStyle from './Person.module.css';
 
 export class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>Hello I'm {this.props.name} and I'm {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changeName} value={this.props.name} />
-            </Aux>
+            </Fragment>
         )
 
 
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const WithClass = (props) => <div className={props.classes}>{props.children}</div>;
+const WithClass = (WrappedComponent, className) => {
+    return props => (
+        <div className={className}>
+            <WrappedComponent />
+        </div>
+    )
+};
 
 export default WithClass

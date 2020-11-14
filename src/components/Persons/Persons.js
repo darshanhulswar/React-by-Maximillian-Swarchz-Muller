f686ee10cbec287ec21b7f632e379d7abc9e1910
Darshan <dhulswar591@gmail.com>
2020-11-14 20:22:09 +0530

    Eliinated Lifecycle Methods

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -10,33 +10,7 @@ export class Persons extends PureComponent {
         }
     }
 
-    static getDerivedStateFromProps(props, state) {
-        console.log('[Persons.js] getDerivedStateFromProps')
-        return null
-    }
-
-    componentWillUnmount() {
-        console.log('[Persons.js] co mponentWillUnmount');
-    }
-
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     console.log('[Persons.js] shouldComponentUpdate');
-    //     if (nextProps.persons !== this.props.persons ||
-    //         nextProps.clicked !== this.props.clicked ||
-    //         nextProps.changed !== this.props.changed) {
-    //         return true
-    //     } else {
-    //         return false
-    //     }
-    // }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('[Persons.js] getSnapshotBeforeUpdate');
-        return { message: 'update' }
-    }
-
     render() {
-        console.log('[Persons.js] render');
         return (
             this.props.persons.map((person, index) => {
                 return <Person
@@ -47,11 +21,6 @@ export class Persons extends PureComponent {
                     changeName={(event) => this.props.changed(event, person.id)} />
             })
         )
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('[Persons.js] componentDidUpdate');
-        console.log(snapshot);
     }
 }
 

cb5a95d71c09406a788b733e2a804556ce76b023
Darshan <dhulswar591@gmail.com>
2020-11-18 00:00:07 +0530

    Higher Order Component [Auxiliary.js] integrated

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,19 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import Auxiliary from '../../../hoc/Auxiliary'
+import WithClass from '../../../hoc/WithClass'
+import classes from './Person.module.css';
 
 export class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Fragment>
+            <Auxiliary>
                 <p onClick={this.props.click}>Hello I'm {this.props.name} and I'm {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changeName} value={this.props.name} />
-            </Fragment>
+            </Auxiliary>
         )
     }
 }
 
-export default Person
+export default WithClass(Person, classes.Person)

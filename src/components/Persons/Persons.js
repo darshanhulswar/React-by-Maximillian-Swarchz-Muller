978aef77fc59be154e84a9f2f4c8d51986def4d9
Darshan <dhulswar591@gmail.com>
2020-11-18 22:22:23 +0530

    cleaned code

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react'
-import Person from './Person/Person'
+import React, { PureComponent } from 'react';
+import Person from './Person/Person';
 
 export class Persons extends PureComponent {
     constructor(props) {
@@ -11,19 +11,21 @@ export class Persons extends PureComponent {
     }
 
     render() {
-        return (
-            this.props.persons.map((person, index) => {
-                return <Person
+        return this.props.persons.map((person, index) => {
+            return (
+                <Person
                     click={() => this.props.clicked(index)}
                     name={person.name}
                     age={person.age}
                     key={person.id}
                     changeName={(event) => this.props.changed(event, person.id)}
-                    isAuth={this.props.isAuthenticated}
                 />
-            })
-        )
+            )
+        })
+
     }
+
 }
+
 
 export default Persons
